633e55b68fce97d622154f940985ec9c7558dea5
hossmi <hossmi@gmail.com>
2019-11-13 21:08:03 +0100

    add enumerable extension methods

--- Blacksmith.Extensions.Enumerables.Tests/UnitTest1.cs
+++ Blacksmith.Extensions.Enumerables.Tests/UnitTest1.cs
@@ -1,4 +1,5 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -8,54 +9,191 @@ namespace Blacksmith.Extensions.Enumerables.Tests
     {
         public string Name { get; set; }
         public int Age { get; set; }
+        public DateTime Registration { get; set; }
+        public string SurName { get; set; }
     }
 
     [TestClass]
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void enumerable_tests()
         {
-            User[] users;
+            User[] sortedUsers, unsortedUsers;
 
-            users = getUsers()
-                .OrderBy(u => u.Name)
-                .ToArray();
+            unsortedUsers = getUsers().ToArray();
 
-            Assert.AreEqual("Florencio", users[0].Name);
-
-            users = getUsers()
+            sortedUsers = getUsers()
                 .sortBy(u => u.Age, OrderDirection.Descendant)
+                .thenBy(u => u.SurName, OrderDirection.Ascendant)
                 .ToArray();
 
-            Assert.AreEqual("Rosa", users[2].Name);
+            Assert.ReferenceEquals(unsortedUsers[0], sortedUsers[3]);
+            Assert.ReferenceEquals(unsortedUsers[1], sortedUsers[1]);
+            Assert.ReferenceEquals(unsortedUsers[2], sortedUsers[0]);
+            Assert.ReferenceEquals(unsortedUsers[3], sortedUsers[2]);
+        }
 
-            users = getUsers()
-                .sortBy(u => u.Age, OrderDirection.Ascendant)
+        [TestMethod]
+        public void queryable_tests()
+        {
+            User[] sortedUsers, unsortedUsers;
+
+            unsortedUsers = getUsers().ToArray();
+
+            sortedUsers = getUsers()
+                .AsQueryable()
+                .sortBy(u => u.Age, OrderDirection.Descendant)
+                .thenBy(u => u.SurName, OrderDirection.Ascendant)
                 .ToArray();
 
-            Assert.AreEqual("Rosa", users[0].Name);
+            Assert.ReferenceEquals(unsortedUsers[0], sortedUsers[3]);
+            Assert.ReferenceEquals(unsortedUsers[1], sortedUsers[1]);
+            Assert.ReferenceEquals(unsortedUsers[2], sortedUsers[0]);
+            Assert.ReferenceEquals(unsortedUsers[3], sortedUsers[2]);
+        }
+
+        [TestMethod]
+        public void notNull_tests()
+        {
+            IEnumerable<User> users;
+            IEnumerable<int> ints;
+
+            users = getUsersWithNulls()
+                .notNull();
+
+            ints = getInts().notNull();
+
+            Assert.AreEqual(4, users.Count());
+            Assert.AreEqual(10, ints.Count());
+        }
+
+        [TestMethod]
+        public void string_enumerables_tests()
+        {
+            string[] strings;
+
+            strings = getStrings()
+                .notNull()
+                .trim()
+                .ToArray();
+
+            Assert.AreEqual(6, strings.Length);
+            Assert.AreEqual("pepe", strings[2]);
+            Assert.AreEqual("tronco", strings[4]);
+
+            strings = strings
+                .notEmpty()
+                .ToArray();
+
+            Assert.AreEqual(2, strings.Length);
+            Assert.AreEqual("pepe", strings[0]);
+            Assert.AreEqual("tronco", strings[1]);
         }
 
         private static IEnumerable<User> getUsers()
         {
+            yield return new User //3
+            {
+                Name = "Narciso",
+                SurName = "Robles Olmos",
+                Age = 21,
+                Registration = new DateTime(2009, 8, 13),
+            };
+
+            yield return new User //1
+            {
+                Name = "Rosa",
+                SurName = "De la huerta Castaños",
+                Age = 55,
+                Registration = new DateTime(2011, 8, 13),
+            };
+
+            yield return new User //0
+            {
+                Name = "Florencio",
+                SurName = "Céspedes Perales",
+                Age = 55,
+                Registration = new DateTime(2015, 8, 13),
+            };
+
+            yield return new User //2
+            {
+                Name = "Florencio",
+                SurName = "Campos Huertas",
+                Age = 34,
+                Registration = new DateTime(2015, 8, 13),
+            };
+        }
+
+        private static IEnumerable<User> getUsersWithNulls()
+        {
             yield return new User
             {
                 Name = "Narciso",
+                SurName = "Robles Olmos",
                 Age = 21,
+                Registration = new DateTime(2009, 8, 13),
             };
+
+            yield return null;
 
             yield return new User
             {
                 Name = "Rosa",
-                Age = 8,
+                SurName = "De la huerta Castaños",
+                Age = 55,
+                Registration = new DateTime(2011, 8, 13),
+            };
+
+            yield return null;
+
+            yield return new User
+            {
+                Name = "Florencio",
+                SurName = "Céspedes Perales",
+                Age = 55,
+                Registration = new DateTime(2015, 8, 13),
             };
 
             yield return new User
             {
                 Name = "Florencio",
-                Age = 55,
+                SurName = "Campos Huertas",
+                Age = 34,
+                Registration = new DateTime(2015, 8, 13),
             };
+
+            yield return null;
+            yield return null;
+            yield return null;
+            yield return null;
+        }
+
+        private static IEnumerable<int> getInts()
+        {
+            yield return 1;
+            yield return 2;
+            yield return 3;
+            yield return 5;
+            yield return 8;
+            yield return 13;
+            yield return 21;
+            yield return 34;
+            yield return 55;
+            yield return 89;
+        }
+
+        private static IEnumerable<string> getStrings()
+        {
+            yield return "";
+            yield return "";
+            yield return "pepe ";
+            yield return null;
+            yield return "";
+            yield return " tronco    ";
+            yield return null;
+            yield return "";
         }
     }
 }
--- Blacksmith.Extensions.Enumerables/Extensions/Enumerables/EnumerableExtensions.cs
+++ Blacksmith.Extensions.Enumerables/Extensions/Enumerables/EnumerableExtensions.cs
@@ -1,13 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 
 namespace Blacksmith
 {
     public static class EnumerableExtensions
     {
-        public static IOrderedEnumerable<TSource> sortBy<TSource, TKey>(this IEnumerable<TSource> source, Func<TSource, TKey> keySelector, OrderDirection direction)
+        public static IOrderedEnumerable<TSource> sortBy<TSource, TKey>(
+            this IEnumerable<TSource> source, Func<TSource, TKey> keySelector, OrderDirection direction)
         {
+            assertNotNull(source, nameof(source));
+            assertNotNull(keySelector, nameof(keySelector));
+
             if (direction == OrderDirection.Ascendant)
                 return source.OrderBy(keySelector);
             else if (direction == OrderDirection.Descendant)
@@ -15,5 +20,171 @@ namespace Blacksmith
             else
                 throw new ArgumentException($"Wrong {nameof(OrderDirection)} enum value");
         }
+
+        public static IOrderedEnumerable<TSource> thenBy<TSource, TKey>(
+            this IOrderedEnumerable<TSource> source, Func<TSource, TKey> keySelector, OrderDirection direction)
+        {
+            assertNotNull(source, nameof(source));
+            assertNotNull(keySelector, nameof(keySelector));
+
+            if (direction == OrderDirection.Ascendant)
+                return source.ThenBy(keySelector);
+            else if (direction == OrderDirection.Descendant)
+                return source.ThenByDescending(keySelector);
+            else
+                throw new ArgumentException($"Wrong {nameof(OrderDirection)} enum value");
+        }
+
+        public static IOrderedQueryable<TSource> sortBy<TSource, TKey>(
+            this IQueryable<TSource> source, Expression<Func<TSource, TKey>> keySelector, OrderDirection direction)
+        {
+            assertNotNull(source, nameof(source));
+            assertNotNull(keySelector, nameof(keySelector));
+
+            if (direction == OrderDirection.Ascendant)
+                return source.OrderBy(keySelector);
+            else if (direction == OrderDirection.Descendant)
+                return source.OrderByDescending(keySelector);
+            else
+                throw new ArgumentException($"Wrong {nameof(OrderDirection)} enum value");
+        }
+
+        public static IOrderedQueryable<TSource> thenBy<TSource, TKey>(
+            this IOrderedQueryable<TSource> source, Expression<Func<TSource, TKey>> keySelector, OrderDirection direction)
+        {
+            assertNotNull(source, nameof(source));
+            assertNotNull(keySelector, nameof(keySelector));
+
+            if (direction == OrderDirection.Ascendant)
+                return source.ThenBy(keySelector);
+            else if (direction == OrderDirection.Descendant)
+                return source.ThenByDescending(keySelector);
+            else
+                throw new ArgumentException($"Wrong {nameof(OrderDirection)} enum value");
+        }
+
+        public static void forEach<TSource>(this IEnumerable<TSource> items, Action<TSource> forEachDelegate)
+        {
+            assertNotNull(items, nameof(items));
+            assertNotNull(items, nameof(forEachDelegate));
+
+            foreach (TSource item in items)
+                forEachDelegate(item);
+        }
+
+        public static IEnumerable<T> whereIf<T>(this IEnumerable<T> items, bool condition, Func<T, bool> predicate)
+        {
+            assertNotNull(items, nameof(items));
+            assertNotNull(predicate, nameof(predicate));
+
+            if (condition)
+                return items.Where(predicate);
+            else
+                return items;
+        }
+
+        public static IQueryable<T> whereIf<T>(this IQueryable<T> items, bool condition, Expression<Func<T, bool>> predicate)
+        {
+            assertNotNull(items, nameof(items));
+            assertNotNull(predicate, nameof(predicate));
+
+            if (condition)
+                return items.Where(predicate);
+            else
+                return items;
+        }
+
+        public static IEnumerable<T> whereIfStringIsFilled<T>(this IEnumerable<T> items, string text, Func<T, bool> predicate)
+        {
+            bool stringIsFilled;
+
+            assertNotNull(items, nameof(items));
+            assertNotNull(predicate, nameof(predicate));
+
+            stringIsFilled = false == string.IsNullOrWhiteSpace(text);
+
+            return whereIf<T>(items, stringIsFilled, predicate);
+        }
+
+        public static IQueryable<T> whereIfStringIsFilled<T>(this IQueryable<T> items, string text, Expression<Func<T, bool>> predicate)
+        {
+            bool stringIsFilled;
+
+            assertNotNull(items, nameof(items));
+            assertNotNull(predicate, nameof(predicate));
+
+            stringIsFilled = false == string.IsNullOrWhiteSpace(text);
+
+            return whereIf<T>(items, stringIsFilled, predicate);
+        }
+
+        public static IQueryable<T> paginate<T>(IQueryable<T> source, int pageSize, int page)
+        {
+            assertNotNull(source, nameof(source));
+
+            return source
+                .Skip(page * pageSize)
+                .Take(pageSize);
+        }
+
+        public static IEnumerable<T> paginate<T>(IEnumerable<T> source, int pageSize, int page)
+        {
+            assertNotNull(source, nameof(source));
+
+            return source
+                .Skip(page * pageSize)
+                .Take(pageSize);
+        }
+
+        public static ICollection<T> push<T>(this ICollection<T> source, T item)
+        {
+            assertNotNull(source, nameof(source));
+
+            source.Add(item);
+            return source;
+        }
+
+        public static IEnumerable<TSource> notNull<TSource>(this IEnumerable<TSource> source)
+        {
+            assertNotNull(source, nameof(source));
+
+            return source.Where(itemNotNull);
+        }
+
+        public static IEnumerable<string> notEmpty(this IEnumerable<string> source)
+        {
+            assertNotNull(source, nameof(source));
+
+            return source.Where(stringIsNotEmpty);
+        }
+
+        public static IEnumerable<string> trim(this IEnumerable<string> source)
+        {
+            assertNotNull(source, nameof(source));
+
+            return source.Select(trimString);
+        }
+
+        private static bool itemNotNull<T>(T item)
+        {
+            return item != null;
+        }
+
+        private static bool stringIsNotEmpty(string source)
+        {
+            return false == string.IsNullOrWhiteSpace(source);
+        }
+
+        private static string trimString(string source)
+        {
+            return source.Trim();
+        }
+
+        private static void assertNotNull(object item, string parameterName)
+        {
+            if (item == null)
+                throw new ArgumentNullException(nameof(parameterName));
+        }
+
     }
 }
