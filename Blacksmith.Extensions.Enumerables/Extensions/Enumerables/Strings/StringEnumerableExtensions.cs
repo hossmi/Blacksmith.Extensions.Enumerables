899d95b37a39eafc5bb43dcbf099035fbb811a06
hossmi <hossmi@gmail.com>
2019-12-30 14:35:31 +0100

    add Enumerable Stringify extesnion method

---
+++ Blacksmith.Extensions.Enumerables/Extensions/Enumerables/Strings/StringEnumerableExtensions.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Blacksmith.Extensions.Enumerables.Strings
+{
+    public static class StringEnumerableExtensions
+    {
+        public const string DEFAULT_STRINGIFY_SEPARATOR = ", ";
+
+        public static string stringify<T>(this IEnumerable<T> items
+            , Func<T, string> selectorFunction = null
+            , string separator = DEFAULT_STRINGIFY_SEPARATOR
+            , bool skipEmptyItems = true)
+        {
+            return prv_stringify(items, selectorFunction, separator, skipEmptyItems);
+        }
+
+        private static string prv_stringify<T>(IEnumerable<T> items, Func<T, string> selectorFunction, string separator, bool skipEmptyItems)
+        {
+            if (items == null)
+                throw new ArgumentNullException(nameof(items));
+            if (separator == null)
+                throw new ArgumentNullException(nameof(separator));
+
+            selectorFunction = selectorFunction ?? prv_toString<T>;
+
+            return items
+                .Select(selectorFunction)
+                .whereIf(skipEmptyItems, prv_stringHasContent)
+                .DefaultIfEmpty(string.Empty)
+                .Aggregate((acum, item) => $"{acum}{separator}{item}");
+        }
+
+        private static bool prv_stringHasContent(string item)
+        {
+            return string.IsNullOrWhiteSpace(item) == false;
+        }
+
+        private static string prv_toString<T>(T item)
+        {
+            return item?.ToString();
+        }
+
+        private static string prv_nulltoString(string item)
+        {
+            return item;
+        }
+    }
+}
